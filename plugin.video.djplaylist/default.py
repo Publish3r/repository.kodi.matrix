2178b06e27f0638eec982346b27bba7c7bbfe43d
Publish3r <62908449+Publish3r@users.noreply.github.com>
2023-02-17 00:37:26 +0100

    update

--- plugin.video.djplaylist/default.py
+++ plugin.video.djplaylist/default.py
@@ -23,6 +23,7 @@ djplaylist = xbmcaddon.Addon('plugin.video.djplaylist')
 addon_icon = 'special://home/addons/plugin.video.djplaylist/icon.png'
 icon_dance = 'special://home/addons/plugin.video.djplaylist/resources/dance.png'
 icon_schlager = 'special://home/addons/plugin.video.djplaylist/resources/schlager.png'
+icon_video = 'special://home/addons/plugin.video.djplaylist/resources/video.png'
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0"}
 
@@ -30,7 +31,7 @@ mode = args.get('mode', None)
 
 def ddp(page):
     r = requests.get(page,headers=headers,timeout=5)
-    match = re.findall('<div class="s-wk">Week</div>(.*?)<div class="charts big mobile"',r.content.decode('utf-8'),re.DOTALL|re.MULTILINE)[0]
+    match = re.findall('<div class="score">(.*?)<div class="charts big mobile"',r.content.decode('utf-8'),re.DOTALL|re.MULTILINE)[0]
     soup = BeautifulSoup(match, 'html.parser')
     platz = soup.find_all(class_="rank")
     bild = soup.find_all(class_="cover")
@@ -76,16 +77,46 @@ def ddp(page):
 
 if mode is None:
 
-    url = build_url({'mode': 'dance', 'foldername': 'DDP - Top 100 - Dance'})
-    li = xbmcgui.ListItem('DDP - Top 100 - Dance')
-    li.setInfo(type='video',infoLabels={'title': 'DDP - Top 100 - Dance', 'plot': 'DDP - Top 100 - Dance'})
+    url = build_url({'mode': 'dance', 'foldername': 'DDP - Dance - Top 100'})
+    li = xbmcgui.ListItem('DDP - Dance - Top 100')
+    li.setInfo(type='video',infoLabels={'title': 'DDP - Dance - Top 100', 'plot': 'DDP - Dance - Top 100'})
+    li.setArt({'fanart': djplaylist.getAddonInfo('fanart'), 'icon': icon_dance, 'thumb' : icon_dance}) 
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+    
+    url = build_url({'mode': 'dance-hot', 'foldername': 'DDP - Dance - Hot 50'})
+    li = xbmcgui.ListItem('DDP - Dance - Hot 50')
+    li.setInfo(type='video',infoLabels={'title': 'DDP - Dance - Hot 50', 'plot': 'DDP - Dance - Hot 50'})
+    li.setArt({'fanart': djplaylist.getAddonInfo('fanart'), 'icon': icon_dance, 'thumb' : icon_dance}) 
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+    
+    url = build_url({'mode': 'dance-neueinsteiger', 'foldername': 'DDP - Dance - Neueinsteiger'})
+    li = xbmcgui.ListItem('DDP - Dance - Neueinsteiger')
+    li.setInfo(type='video',infoLabels={'title': 'DDP - Dance - Neueinsteiger', 'plot': 'DDP - Dance - Neueinsteiger'})
     li.setArt({'fanart': djplaylist.getAddonInfo('fanart'), 'icon': icon_dance, 'thumb' : icon_dance}) 
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
 
-    url = build_url({'mode': 'schlager', 'foldername': 'DDP - Top 100 - Schlager'})
-    li = xbmcgui.ListItem('DDP - Top 100 - Schlager')
-    li.setInfo(type='video',infoLabels={'title': 'DDP - Top 100 - Schlager', 'plot': 'DDP - Top 100 - Schlager'})
+    url = build_url({'mode': 'schlager', 'foldername': 'DDP - Schlager - Top 100'})
+    li = xbmcgui.ListItem('DDP - Schlager - Top 100')
+    li.setInfo(type='video',infoLabels={'title': 'DDP - Schlager - Top 100', 'plot': 'DDP - Schlager - Top 100'})
     li.setArt({'fanart': djplaylist.getAddonInfo('fanart'), 'icon': icon_schlager, 'thumb' : icon_schlager})
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+    
+    url = build_url({'mode': 'schlager-hot', 'foldername': 'DDP - Schlager - Hot 50'})
+    li = xbmcgui.ListItem('DDP - Schlager - Hot 50')
+    li.setInfo(type='video',infoLabels={'title': 'DDP - Schlager - Hot 50', 'plot': 'DDP - Schlager - Hot 50'})
+    li.setArt({'fanart': djplaylist.getAddonInfo('fanart'), 'icon': icon_schlager, 'thumb' : icon_schlager})
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+    
+    url = build_url({'mode': 'schlager-neueinsteiger', 'foldername': 'DDP - Schlager - Neueinsteiger'})
+    li = xbmcgui.ListItem('DDP - Schlager - Neueinsteiger')
+    li.setInfo(type='video',infoLabels={'title': 'DDP - Schlager - Neueinsteiger', 'plot': 'DDP - Schlager - Neueinsteiger'})
+    li.setArt({'fanart': djplaylist.getAddonInfo('fanart'), 'icon': icon_dance, 'thumb' : icon_schlager}) 
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+    
+    url = build_url({'mode': 'video-top30', 'foldername': 'DDP - Video - Top 30'})
+    li = xbmcgui.ListItem('DDP - Video - Top 30')
+    li.setInfo(type='video',infoLabels={'title': 'DDP - Video - Top 30', 'plot': 'DDP - Video - Top 30'})
+    li.setArt({'fanart': djplaylist.getAddonInfo('fanart'), 'icon': icon_dance, 'thumb' : icon_video}) 
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
 
     xbmcplugin.endOfDirectory(addon_handle)
@@ -95,6 +126,21 @@ elif mode[0] == 'dance':
 
 elif mode[0] == 'schlager':
     ddp("https://www.dj-playlist.de/top-100/schlager")
+    
+elif mode[0] == 'dance-hot':
+    ddp("https://www.dj-playlist.de/hot-50/dance")
+
+elif mode[0] == 'schlager-hot':
+    ddp("https://www.dj-playlist.de/hot-50/schlager")
+    
+elif mode[0] == 'dance-neueinsteiger':
+    ddp("https://www.dj-playlist.de/neueinsteiger/dance")
+
+elif mode[0] == 'schlager-neueinsteiger':
+    ddp("https://www.dj-playlist.de/neueinsteiger/schlager")
+    
+elif mode[0] == 'video-top30':
+    ddp("https://www.dj-playlist.de/top-30/video")
     
 elif mode[0] == 'error':
     xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%('[B]Error[/B]', 'Video not available.', 5000, addon_icon))
