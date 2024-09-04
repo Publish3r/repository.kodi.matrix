25cb015b983f5bb710e202ba080d625cdd4dc356
Publish3r <62908449+Publish3r@users.noreply.github.com>
2024-09-04 16:32:24 +0200

    plugin.video.sportstribal

--- plugin.video.sportstribal/default.py
+++ plugin.video.sportstribal/default.py
@@ -8,7 +8,6 @@ import xbmcaddon
 import requests
 import json
 import re
-from datetime import datetime
 
 base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
@@ -18,139 +17,46 @@ addon = xbmcaddon.Addon()
 mode = args.get('mode', None)
 
 sportstribal = xbmcaddon.Addon('plugin.video.sportstribal')
-epg = addon.getSetting("epg")
-geoblock = addon.getSetting("geo")
-hide = addon.getSetting("hide")
 
 addon_icon = 'special://home/addons/plugin.video.sportstribal/icon.png'
 addon_fanart = 'special://home/addons/plugin.video.sportstribal/fanart.jpg'
 
-bearer = requests.get('https://raw.githubusercontent.com/Publish3r/repository.kodi.matrix/main/plugin.video.sportstribal/resources/bearer.txt', timeout=5).text
-bearer = str(bearer)
-
-UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0'
-REF = 'https://watch.sportstribal.tv/'
-
-s_headers = {
-    'User-Agent': UA,
-    'Referer': REF,
-    'authorization': bearer,
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
+    'Origin': 'https://www.freelivesports.tv',
+    'DNT': '1',
+    'Sec-GPC': '1',
+    'Connection': 'keep-alive',
+    'Referer': 'https://www.freelivesports.tv/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'cross-site',
 }
 
-s_params = {
-    'mute': 'false',
-    'consent': '',
-    'gdprOptin': 'true',
-}
-
-c_headers = {
-    'Referer': REF,
-    'User-Agent': UA,
-}
-
-c_params = {
-    'X-Response-Version': '3.0',
-}
-
-e_params = {
-    'includeDetails': 'false',
-    'X-Response-Version': '3.0',
+params = {
+    '__site': 'freelivesports',
+    '__source': 'web',
 }
 
 def build_url(query):
     return base_url + '?' + urllib.parse.urlencode(query)
 
-def get_stream(channelid):
-    try:
-        r = requests.get(
-            f'https://exposureapi.emp.ebsd.ericsson.net/v2/customer/SportsTribal/businessunit/Prod/entitlement/{channelid}/play',
-            params=s_params,
-            headers=s_headers,
-            timeout=5,
-        )
-        json_data = json.loads(r.content.decode())
-        stream = json_data['formats'][0]['mediaLocator']
-        return stream
-    except:
-        xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%('[B]Error[/B]', 'Stream is not available.', 5000, addon_icon))
-        stream = "error"
-        return stream
-
 def get_channels():
-    r1 = requests.get('https://api.watch.sportstribal.tv/channels/list', params=c_params, headers=c_headers, timeout=5)
-    json_data1 = json.loads(r1.content.decode())
-    if epg == "true":
-        r2 = requests.get('https://api.watch.sportstribal.tv/guide/channels/nownext', params=e_params, headers=c_headers, timeout=5)
-        json_data2 = json.loads(r2.content.decode())
-    for i in json_data1:
-        channelid = i['ID']
+    r = requests.get('https://epg.unreel.me/v2/sites/freelivesports/live-channels/public/90e79153782dc65c49b824d17b2dcb56?__site=freelivesports&__source=web', params=params, headers=headers, timeout=5)
+    json_data = json.loads(r.content.decode())
+    for i in json_data:
         name = i['name']
-        desc = i['summary']
-        if geoblock == "true":
-            geo = ""
-            try:
-                if "*Available" in i['summary']:
-                    match = re.findall('Available(.*?)nly',str(desc),re.DOTALL|re.MULTILINE)[0]
-                    geo = f"*Available{match}nly."
-                    name = f"{name}[COLOR red]*[/COLOR]"
-            except:
-                pass
-        if epg == "true":
-            try:
-                epg_now = json_data2[channelid][0]['title']
-                epg_now_start = json_data2[channelid][0]['start']
-                epg_now_stop = json_data2[channelid][0]['end']
-                epg_now_start = int(epg_now_start)
-                epg_now_stop = int(epg_now_stop)
-                epg_now_start = datetime.fromtimestamp(epg_now_start).strftime('%H:%M')
-                epg_now_stop = datetime.fromtimestamp(epg_now_stop).strftime('%H:%M')
-                time_now = f"{epg_now_start} - {epg_now_stop}"
-                epg_next = json_data2[channelid][1]['title']
-                epg_next_start = json_data2[channelid][1]['start']
-                epg_next_stop = json_data2[channelid][1]['end']
-                epg_next_start = int(epg_next_start)
-                epg_next_stop = int(epg_next_stop)
-                epg_next_start = datetime.fromtimestamp(epg_next_start).strftime('%H:%M')
-                epg_next_stop = datetime.fromtimestamp(epg_next_stop).strftime('%H:%M')
-                time_next = f"{epg_next_start} - {epg_next_stop}"
-                desc = f"[COLOR green][B]{time_now}[/B][/COLOR][CR][COLOR yellow]{epg_now}[/COLOR][CR][COLOR green][B]{time_next}[/B][/COLOR][CR][COLOR yellow]{epg_next}[/COLOR]" 
-                if geoblock == "true":
-                    try:
-                        if geo != "":
-                            geo = geo.replace("Only", "only") 
-                            geo = geo.replace(geo, f"[COLOR red]{geo}[/COLOR]")
-                            desc = f"{desc}[CR][CR]{geo}" 
-                    except:
-                        pass                    
-            except:
-                desc = i['summary']
-                if geoblock == "true":
-                    try:
-                        desc = desc.replace(geo, f"[COLOR red]{geo}[/COLOR]")
-                        desc = desc.replace("Only", "only")
-                    except:
-                        pass
-        else:
-            desc = i['summary']
-            if geoblock == "true":
-                try:
-                    desc = desc.replace(geo, f"[COLOR red]{geo}[/COLOR]")
-                    desc = desc.replace("Only", "only")
-                except:
-                    pass
-        logo = i['images']['logo']
-        url = build_url({'mode': 'play', 'channelid': channelid, 'name': name, 'desc': desc, 'logo': logo})
-        if hide == "true":
-            if not "*Available" in desc:
-                li = xbmcgui.ListItem(name)
-                li.setInfo('Video', {"title": name, "plot": desc})
-                li.setArt({'fanart': addon_fanart, 'icon': logo, 'thumb' : logo}) 
-                xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
-        else:
-            li = xbmcgui.ListItem(name)
-            li.setInfo('Video', {"title": name, "plot": desc})
-            li.setArt({'fanart': addon_fanart, 'icon': logo, 'thumb' : logo}) 
-            xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
+        desc = i['description']
+        logo = i['thumbnail']
+        stream = i['url']
+        stream = stream.split("?")[0]        
+        url = build_url({'mode': 'play', 'stream': stream, 'name': name, 'desc': desc, 'logo': logo})
+        li = xbmcgui.ListItem(name)
+        li.setInfo('Video', {"title": name, "plot": desc})
+        li.setArt({'fanart': addon_fanart, 'icon': logo, 'thumb' : logo}) 
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
     xbmcplugin.endOfDirectory(addon_handle, cacheToDisc=False)
     
 def play(stream, name, desc, logo):
@@ -166,18 +72,8 @@ if mode is None:
     get_channels()
         
 elif mode[0] == "play":
-    channelid = args['channelid'][0]
+    stream = args['stream'][0]
     name = args['name'][0]
-    if geoblock == "true":
-        try:
-            if "*" in name:
-                name = name.replace("[COLOR red]*[/COLOR]", "")
-        except:
-            pass
     desc = args['desc'][0]
     logo = args['logo'][0]
-    stream = get_stream(channelid)
-    if stream == "error":
-        xbmc.executebuiltin("Container.Refresh")
-    else:
-        play(stream, name, desc, logo)
+    play(stream, name, desc, logo)
